7d7d69fc0abf56a7434f419e2262a263f427620c
Nick Tillinghast <nickt2@cox.net>
2019-10-25 09:07:44 -0700

    commit check

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const massive = require("massive");
+const express = require("express");
+// const controller = require("./controller");
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const app = express();
+
+app.use(express.json());
+
+massive(CONNECTION_STRING).then(db => {
+  console.log("connected to db");
+  app.set("db", db);
+});
+
+// app.use(
+//   session({
+//     secret: SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24
+//     }
+//   })
+// );
+
+app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
